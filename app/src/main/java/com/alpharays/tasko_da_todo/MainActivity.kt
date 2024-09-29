0b9b51f5a1dc0f0ea10e0b471ee0d047b35fed03
stkrr002 <sumitkrr2002@gmail.com>
2024-09-29 20:16:00 +0530

    dialog design work done

--- app/src/main/java/com/alpharays/tasko_da_todo/MainActivity.kt
+++ app/src/main/java/com/alpharays/tasko_da_todo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +18,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
--- app/src/main/java/com/alpharays/tasko_da_todo/presentation/featuretodolist/dialog/EditTaskDialog.kt
+++ app/src/main/java/com/alpharays/tasko_da_todo/presentation/featuretodolist/dialog/EditTaskDialog.kt
@@ -1,54 +1,91 @@
 package com.alpharays.tasko_da_todo.presentation.featuretodolist.dialog
 
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.alpharays.tasko_da_todo.data.entity.Task
 
 @Composable
 fun EditTaskDialog(task: Task, onSubmitClicked: (Task) -> Unit, onCancelClicked: () -> Unit) {
     var taskTitle by remember { mutableStateOf(task.title) }
     var taskDescription by remember { mutableStateOf(task.description) }
+
     AlertDialog(
-        onDismissRequest = { },
+        onDismissRequest = { onCancelClicked() },
         title = { Text(text = "Add Task") },
         text = {
             Column {
-                TextField(
+                OutlinedTextField(
                     value = taskTitle,
                     onValueChange = { taskTitle = it },
-                    label = { Text("Title") }
-                )
-                TextField(
+                    label = { Text("Title") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
+
+                    )
+                OutlinedTextField(
                     value = taskDescription,
                     onValueChange = { taskDescription = it },
-                    label = { Text("Description") }
+                    label = { Text("Description") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp),
                 )
             }
         },
         confirmButton = {
-            Button(
-                onClick = {
-                    if (taskTitle.isNotBlank() && taskDescription.isNotBlank()) {
-                        onSubmitClicked(task.copy(title = taskTitle, description = taskDescription))
-                    }
-
-                }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
             ) {
+                Button(
+                    onClick = { onCancelClicked() },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("Cancel")
+                }
+                Spacer(modifier = Modifier.width(8.dp))
+                val context = LocalContext.current
 
-                Text("Submit")
-            }
-        },
-        dismissButton = {
-            Button(onClick = { onCancelClicked() }) {
-                Text("Cancel")
+                Button(
+                    onClick = {
+                        if (taskTitle.isNotBlank() && taskDescription.isNotBlank()) {
+                            onSubmitClicked(
+                                task.copy(
+                                    title = taskTitle,
+                                    description = taskDescription
+                                )
+                            )
+                        } else {
+                            Toast.makeText(context, "Please fill all fields", Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("Submit")
+                }
             }
         }
     )
