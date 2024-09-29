578952a46d6020aa4a61a7237628f4abcf7d20b9
stkrr002 <sumitkrr2002@gmail.com>
2024-09-29 20:20:50 +0530

    mapper and domain created

---
+++ app/src/main/java/com/alpharays/tasko_da_todo/domain/mapper/TaskDataMapper.kt
@@ -0,0 +1,21 @@
+package com.alpharays.tasko_da_todo.domain.mapper
+
+import com.alpharays.tasko_da_todo.data.entity.Task
+import com.alpharays.tasko_da_todo.domain.model.TaskData
+
+fun Task.toTaskData(): TaskData {
+    return TaskData(
+        title = this.title,
+        description = this.description,
+        position = this.position
+    )
+}
+
+fun TaskData.toTask(id: Int = 0): Task {
+    return Task(
+        id = id,
+        title = this.title,
+        description = this.description,
+        position = this.position
+    )
+}
---
+++ app/src/main/java/com/alpharays/tasko_da_todo/domain/model/TaskData.kt
@@ -0,0 +1,8 @@
+package com.alpharays.tasko_da_todo.domain.model
+
+
+data class TaskData(
+    val title: String,
+    val description: String,
+    val position: Int
+)
