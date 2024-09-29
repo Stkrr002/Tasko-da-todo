486615c7bbbbac07110d3c83baee245efdb35118
stkrr002 <sumitkrr2002@gmail.com>
2024-09-29 13:00:23 +0530

    mid commit

--- app/src/main/java/com/alpharays/tasko_da_todo/MainActivity.kt
+++ app/src/main/java/com/alpharays/tasko_da_todo/MainActivity.kt
@@ -3,38 +3,28 @@ package com.alpharays.tasko_da_todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.alpharays.tasko_da_todo.presentation.TaskViewModel
-import com.alpharays.tasko_da_todo.ui.theme.TaskodatodoTheme
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.alpharays.tasko_da_todo.data.entity.Task
+import com.alpharays.tasko_da_todo.presentation.TaskViewModel
 import com.alpharays.tasko_da_todo.presentation.dragdrop.DragDropColumn
+import com.alpharays.tasko_da_todo.ui.theme.TaskodatodoTheme
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 @AndroidEntryPoint
@@ -77,11 +67,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun ToDoListApp(innerPadding: PaddingValues) {
-
-
-}
 
 @Composable
 fun DragNDropItemsList(
